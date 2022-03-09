7d2fc0ebb8de6e87bae2aed0a99e6b1c87708110
chardon55 <charlesdong_2000@outlook.com>
2022-03-09 18:49:49 +0800

    Update code

--- cuda_test.py
+++ cuda_test.py
@@ -2,22 +2,23 @@ import torch
 from torch import cuda
 
 
-def test_cuda(verbose=False) -> bool:
+def test_cuda(verbose=False) -> torch.device:
     cuda_available = cuda.is_available()
-    if not verbose:
-        return cuda_available
+    available_device = torch.device("cuda" if cuda_available else "cpu")
 
-    print("CUDA supported?: " + 'Yes' if cuda_available else 'No')
-    print("Current CUDA version: " + torch.version.cuda if cuda_available else "n/a")
-    print("-" * 20)
+    if verbose:
+        print("CUDA supported?: " + 'Yes' if cuda_available else 'No')
+        print("Current CUDA version: " +
+              torch.version.cuda if cuda_available else "n/a")
+        print("-" * 20)
 
-    if cuda_available:
-        for i in range(cuda.device_count()):
-            dp = cuda.get_device_properties(i)
-            print(f"GPU {i}:\t{dp.name}")
-            print(f"\t\tMinimal CUDA version: {dp.major}.{dp.minor}")
+        if cuda_available:
+            for i in range(cuda.device_count()):
+                dp = cuda.get_device_properties(i)
+                print(f"GPU {i}:\t{dp.name}")
+                print(f"\t\tOldest CUDA compatibility: {dp.major}.{dp.minor}")
 
-    return cuda_available
+    return available_device
 
 
 def main():
---
+++ q.py
@@ -0,0 +1,9 @@
+from cuda_test import test_cuda
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
