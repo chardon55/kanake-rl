842fc472f5c5f93310ae0617249ce608cf234dc2
chardon55 <charlesdong_2000@outlook.com>
2022-04-08 15:22:32 +0800

    Update cuda_test.py

--- cuda_test.py
+++ cuda_test.py
@@ -2,7 +2,7 @@ import torch
 from torch import cuda
 
 
-def test_cuda(verbose=False) -> torch.device:
+def test_cuda(verbose=False, rand_test=False) -> torch.device:
     cuda_available = cuda.is_available()
     available_device = torch.device("cuda" if cuda_available else "cpu")
 
@@ -16,13 +16,19 @@ def test_cuda(verbose=False) -> torch.device:
             for i in range(cuda.device_count()):
                 dp = cuda.get_device_properties(i)
                 print(f"GPU {i}:\t{dp.name}")
-                print(f"\t\tOldest CUDA compatibility: {dp.major}.{dp.minor}")
+                print(f"\t\tCUDA compatibility: {dp.major}.{dp.minor}")
+
+    if rand_test:
+        print("Generating a 5x5 tensor... ", end='')
+        gr = torch.rand(5, 5, device='cuda')
+        print("done")
+        print(gr)
 
     return available_device
 
 
 def main():
-    test_cuda(verbose=True)
+    test_cuda(verbose=True, rand_test=True)
 
 
 if __name__ == '__main__':
