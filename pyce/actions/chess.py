33887a2c4b8de84f7e795558a9158d907bdfdf62
chardon55 <charlesdong_2000@outlook.com>
2022-05-04 17:40:24 +0800

    Update code

--- pyce/actions/chess.py
+++ pyce/actions/chess.py
@@ -1,10 +1,10 @@
-from .action import Action
+from .actionset import ActionSet
 
 
 RANGES = (0, 23, 31, 47, 55, 63, 71, 73)
 
 
-class ChessAction(Action):
+class ChessAction(ActionSet):
     def _select_piece(self, action: int) -> tuple[2]:
         piece, c = 0, 0
         # TODO
